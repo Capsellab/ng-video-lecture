9d5d26d0e8a4a3d08df7facc1b31897ca26b0481
Capsellab <capsellab.info@gmail.com>
2023-05-14 07:54:32 +0900

    Added step-by-step code along original Youtube timeline.

--- bigram4.py
+++ bigram4.py
@@ -5,11 +5,12 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
-max_iters = 3000
+max_iters = 5000 # Inserting a single self-attention block to our network: 4
 eval_interval = 300
-learning_rate = 1e-2
+learning_rate = 1e-3 # Inserting a single self-attention block to our network: 4
 device = 'cuda' if torch.cuda.is_available() else 'cpu' # New things Number 1: GPU
 eval_iters = 200
+n_embd = 32 # Minor code cleanup: 2
 # ------------
 
 torch.manual_seed(1337)
@@ -68,18 +69,76 @@ def estimate_loss(): # New things Number 2: average loss function
     model.train()
     return out
 
+class Head(nn.Module): # Inserting a single self-attention block to our network: 1
+    """ one head of self-attention """
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x)                                 # (B,T,C)
+        q = self.query(x)                               # (B,T,C)
+        # compute attention scores ("affinities")
+        wei =  q @ k.transpose(-2, -1) * C**-0.5        # (B,T,C) @ (B,C,T) --> (B,T,T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B,T,T)
+        wei = F.softmax(wei, dim=-1)                    # (B,T,T)
+        # perform the weighted aggregation of the values
+        v = self.value(x)                               # (B,T,C)
+        out = wei @ v                                   # (B,T,T) @ (B,T,C) --> (B,T,C)
+        return out
+
+class MultiHeadAttention(nn.Module): # Multi-headed self-attention: 1
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
+class FeedFoward(nn.Module): # Feedforward layers of transformer: 1
+    """ a simple linear layer followed by a non-linearity"""
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+                nn.Linear(n_embd, n_embd),
+                nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size):
+    def __init__(self): # Minor code cleanup: 1
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd) # Minor code cleanup: 2
+        self.position_embedding_table = nn.Embedding(block_size, n_embd) # Positional encoding: 1
+        #self.sa_head = Head(n_embd) # Inserting a single self-attention block to our network: 2
+        self.sa_heads = MultiHeadAttention(4, n_embd//4) # i.e. 4 heads of 8-dimensional self-attention # Multi-headed self-attention: 2
+        self.ffwd = FeedFoward(n_embd) # Feedforward layers of transformer: 2
+        self.lm_head = nn.Linear(n_embd, vocab_size) # Minor code cleanup: 3
 
     def forward(self, idx, targets=None):
+        B, T = idx.shape # Positional encoding: 2
 
         # idx and targets are both (B,T) tensor of integers
-        logits = self.token_embedding_table(idx) # (B,T,C)
+        tok_emb = self.token_embedding_table(idx) # (B,T,C) # Minor code cleanup: 3
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C) # Positional encoding: 2
+        x = tok_emb + pos_emb # (B,T,C) + (1,T,C) --> (B,T,C) # Positional encoding: 2
+        #x = self.sa_head(x) # apply one head of self-attention. (B,T,C) # Inserting a single self-attention block to our network: 2
+        x = self.sa_heads(x) # apply multi heads of self-attention. (B,T,C) # Multi-headed self-attention: 2
+        x = self.ffwd(x) # (B,T,C) # Feedforward layers of transformer: 2
+        logits = self.lm_head(x) # (B,T,vocab_size) # Positional encoding: 2
 
         if targets is None:
             loss = None
@@ -94,19 +153,21 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:] # Inserting a single self-attention block to our network: 3
             # get the predictions (do forward function)
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond) # Inserting a single self-attention block to our network: 3
             # focus only on the last time step
-            logits = logits[:, -1, :] # becomes (B, C)
+            logits = logits[:, -1, :] # becomes (B,C)
             # apply softmax to get probabilities
-            probs = F.softmax(logits, dim=-1) # (B, C)
+            probs = F.softmax(logits, dim=-1) # (B,C)
             # sample from the distribution
-            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            idx_next = torch.multinomial(probs, num_samples=1) # (B,1)
             # append sampled index to the running sequence
-            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+            idx = torch.cat((idx, idx_next), dim=1) # (B,T+1)
         return idx
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel() # Minor code cleanup: 1
 m = model.to(device) # New things Number 1: GPU
 
 # create a PyTorch optimizer
